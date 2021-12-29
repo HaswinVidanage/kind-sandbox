992a531211bc055eb9f3f53b99d8ec55ae25165f
Haswin Vidanage <haswin.v@ntucfirstcampus.com>
2021-12-29 11:35:15 +0800

    * fix sed command for mac

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 kind create cluster --config ./kind-multi-node.yml --kubeconfig ./kubeconfig
 kind get kubeconfig --name=kind > ./kubeconfig 
-sed -i 's%server: https://127.0.0.1:[0-9]*%server: https://127.0.0.1:6443%g' ./kubeconfig
+sed -i '' 's%server: https://127.0.0.1:[0-9]*%server: https://127.0.0.1:6443%g' ./kubeconfig
 docker exec -it kind-control-plane sh -c "apt-get update && apt-get install -y vim bash-completion && mkdir -p /root/.kube"
 docker exec -it kind-control-plane bash -c "echo 'source /usr/share/bash-completion/bash_completion' >>~/.bashrc"
 docker exec -it kind-control-plane bash -c "echo 'source <(kubectl completion bash)' >>~/.bashrc"
